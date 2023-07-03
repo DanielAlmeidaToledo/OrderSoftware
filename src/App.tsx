1c841d153e9d23033c38bafdbc0c76ef7a35f1d2
Daniel Toledo <dan992965@gmail.com>
2023-07-03 11:38:59 -0300

    feature: cria rotas iniciais com react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
-const App = () => <h1>App</h1>;
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { MainPage } from "./pages/main";
+import { Login } from "./pages/login";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <MainPage />,
+    children: [
+      {
+        path: "/rota1",
+        element: <div>Rota 1</div>,
+      },
+      {
+        path: "/rota2",
+        element: <div>Rota 2</div>,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/pages/main/main.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => (
+  <>
+    <h1>Main</h1>
+    <Outlet />
+  </>
+);
+
+export default Main;
