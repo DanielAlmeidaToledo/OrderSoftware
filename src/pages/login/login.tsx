7438209ff77d4cbb3ca282f77cbb41ee594880d6
Daniel Toledo <dan992965@gmail.com>
2023-07-05 13:20:08 -0300

    feature: adiciona tela de login inicial

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,37 @@
-import { LoginWrapper } from "./style";
+import {
+  LoginWrapper,
+  Form,
+  ButtonGithub,
+  ButtonSubmit,
+  Division,
+} from "./style";
+
+import GithubLogo from "../../assets/github.svg";
+import Logo from "../../assets/logo.svg";
 
 const Login = () => (
   <LoginWrapper>
-    <h1>Login</h1>
+    <img src={Logo} alt="Logo" />
+    <Form>
+      <ButtonGithub type="button">
+        <img src={GithubLogo} alt="Github Logo" />
+        Entrar com Github
+      </ButtonGithub>
+      <Division>
+        <hr />
+        <span>ou</span>
+        <hr />
+      </Division>
+      <div>
+        <label htmlFor="username">Email</label>
+        <input type="email" placeholder="Entre com seu email" />
+      </div>
+      <div>
+        <label htmlFor="password">Senha</label>
+        <input type="password" placeholder="Entre com sua senha" />
+      </div>
+      <ButtonSubmit type="submit">Entrar</ButtonSubmit>
+    </Form>
   </LoginWrapper>
 );
 
