f29cfc65307e6740c5ef109e3d45844bcf2bf53c
Daniel Toledo <dan992965@gmail.com>
2023-07-05 13:19:49 -0300

    feature: adiciona estilizacoes e variaveis globais

---
+++ src/styles/variables.tsx
@@ -0,0 +1,10 @@
+export const variables = {
+  colors: {
+    primary: "#F23E79",
+    textPrimary: "#FAFAFB",
+    textSecondary: "#FFFFFF",
+    textLight: "#AEB0BF",
+    backgroundPrimary: "#1F254E",
+    backgroundSecondary: "#0F1436",
+  },
+};
