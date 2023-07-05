711578c352dabf5d941fb3e751a79db72621f5e3
Daniel Toledo <dan992965@gmail.com>
2023-07-04 22:48:44 -0300

    feature: cria estilizacao inicial

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from "./App.tsx";
+import "./styles/global.css";
+
 import ErrorBoundary from "./components/error.tsx";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
---
+++ src/pages/login/style.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  color: #fff;
+  background-image: linear-gradient(to top, #12173e 0%, #191f4b 100%);
+`;
