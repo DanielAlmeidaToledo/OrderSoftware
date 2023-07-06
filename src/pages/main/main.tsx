4fc791ab3bb229ba7a05842c709f28078c7dd79f
Daniel Toledo <dan992965@gmail.com>
2023-07-05 23:11:22 -0300

    feature: cria e ajusta o firebase

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 
-const Main = () => (
-  <>
-    <h1>Main</h1>
-    <Outlet />
-  </>
-);
-
-export default Main;
+export default function MainPage() {
+  return (
+    <>
+      <h1>Main</h1>
+      <Outlet />
+    </>
+  );
+}
