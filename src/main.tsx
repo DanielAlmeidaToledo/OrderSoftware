f4fb6d51f4f00aca4b8bab3234ae97c4a7545f0b
Daniel Toledo <dan992965@gmail.com>
2023-08-08 23:38:57 -0300

    feature: adiciona context e move função de login

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import "./styles/global.css";
 
 import ErrorBoundary from "./components/error.tsx";
+import AuthProvider from "./contexts/auth.tsx";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <AuthProvider>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </AuthProvider>
   </React.StrictMode>
 );
